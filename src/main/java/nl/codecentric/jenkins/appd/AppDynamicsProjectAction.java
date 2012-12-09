19f1103741ee34f1a43c5d34ad5e77ec6e27920b
Miel Donkers <miel.donkers@codecentric.nl>
2012-12-09 17:35:35 +0100

    Also got the project action working, retrieving previous reports

--- src/main/java/nl/codecentric/jenkins/appd/AppDynamicsProjectAction.java
+++ src/main/java/nl/codecentric/jenkins/appd/AppDynamicsProjectAction.java
@@ -9,6 +9,7 @@ import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -45,55 +46,121 @@ public class AppDynamicsProjectAction implements Action {
     return "graph.gif";
   }
 
+  public List<String> getPerformanceReportList() {
+    List<String> testList = new ArrayList<String>();
 
-    /*
-    public void doErrorsGraph(StaplerRequest request, StaplerResponse response)
-            throws IOException {
-        PerformanceReportPosition performanceReportPosition = new PerformanceReportPosition();
-        request.bindParameters(performanceReportPosition);
-        String performanceReportNameFile = performanceReportPosition.getPerformanceReportPosition();
-        if (performanceReportNameFile == null) {
-            if (getPerformanceReportList().size() == 1) {
-                performanceReportNameFile = getPerformanceReportList().get(0);
-            } else {
-                return;
-            }
-        }
-        if (ChartUtil.awtProblemCause != null) {
-            // not available. send out error message
-            response.sendRedirect2(request.getContextPath() + "/images/headless.png");
-            return;
-        }
-        DataSetBuilder<String, ChartUtil.NumberOnlyBuildLabel> dataSetBuilderErrors = new DataSetBuilder<String, ChartUtil.NumberOnlyBuildLabel>();
-        List<? extends AbstractBuild<?, ?>> builds = getProject().getBuilds();
-        Range buildsLimits = getFirstAndLastBuild(request, builds);
+    DataSetBuilder<String, ChartUtil.NumberOnlyBuildLabel> dataSet = new DataSetBuilder<String, ChartUtil.NumberOnlyBuildLabel>();
+    List<? extends AbstractBuild<?, ?>> builds = project.getBuilds();
 
-        int nbBuildsToAnalyze = builds.size();
-        for (AbstractBuild<?, ?> currentBuild :builds) {
-            if (buildsLimits.in(nbBuildsToAnalyze)) {
+    int nbBuildsToAnalyze = builds.size();
+    for (AbstractBuild<?, ?> currentBuild : builds) {
+      ChartUtil.NumberOnlyBuildLabel label = new ChartUtil.NumberOnlyBuildLabel(currentBuild);
+      AppDynamicsBuildAction performanceBuildAction = currentBuild.getAction(AppDynamicsBuildAction.class);
+      if (performanceBuildAction == null) {
+        continue;
+      }
+      AppDynamicsReport report = null;
+      report = performanceBuildAction.getBuildActionResultsDisplay().getAppDynamicsReport();
+      if (report == null) {
+        nbBuildsToAnalyze--;
+        continue;
+      }
 
-                if (!buildsLimits.includedByStep(currentBuild.number)){
-                    continue;
-                }
-
-                ChartUtil.NumberOnlyBuildLabel label = new ChartUtil.NumberOnlyBuildLabel(currentBuild);
-                PerformanceBuildAction performanceBuildAction = currentBuild.getAction(PerformanceBuildAction.class);
-                if (performanceBuildAction == null) {
-                    continue;
-                }
-                PerformanceReport performanceReport = performanceBuildAction.getPerformanceReportMap().getPerformanceReport(
-                        performanceReportNameFile);
-                if (performanceReport == null) {
-                    nbBuildsToAnalyze--;
-                    continue;
-                }
-                dataSetBuilderErrors.add(performanceReport.errorPercent(),
-                        Messages.ProjectAction_Errors(), label);
-            }
-            nbBuildsToAnalyze--;
-        }
-        ChartUtil.generateGraph(request, response,
-                createErrorsChart(dataSetBuilderErrors.build()), 400, 200);
+      testList.add("report " + label.toString() + " errors: " + report.errorPercent());
     }
-    */
+
+    return testList;
+  }
+
+
+  private DataSetBuilder getTrendReportData(final StaplerRequest request,
+                                            String performanceReportNameFile) {
+
+    DataSetBuilder<String, ChartUtil.NumberOnlyBuildLabel> dataSet = new DataSetBuilder<String, ChartUtil.NumberOnlyBuildLabel>();
+    List<? extends AbstractBuild<?, ?>> builds = project.getBuilds();
+
+    int nbBuildsToAnalyze = builds.size();
+    for (AbstractBuild<?, ?> currentBuild : builds) {
+        ChartUtil.NumberOnlyBuildLabel label = new ChartUtil.NumberOnlyBuildLabel(currentBuild);
+        AppDynamicsBuildAction performanceBuildAction = currentBuild.getAction(AppDynamicsBuildAction.class);
+        if (performanceBuildAction == null) {
+          continue;
+        }
+        AppDynamicsReport report = null;
+        report = performanceBuildAction.getBuildActionResultsDisplay().getAppDynamicsReport();
+        if (report == null) {
+          nbBuildsToAnalyze--;
+          continue;
+        }
+      /*
+        dataSet.add(Math.round(report.getAverage()),
+            Messages.ProjectAction_Average(), label);
+        dataSet.add(Math.round(report.getMedian()),
+            Messages.ProjectAction_Median(), label);
+        dataSet.add(Math.round(report.get90Line()),
+            Messages.ProjectAction_Line90(), label);
+        dataSet.add(Math.round(report.getMin()),
+            Messages.ProjectAction_Minimum(), label);
+        dataSet.add(Math.round(report.getMax()),
+            Messages.ProjectAction_Maximum(), label);
+        dataSet.add(Math.round(report.errorPercent()),
+            Messages.ProjectAction_PercentageOfErrors(), label);
+        dataSet.add(Math.round(report.countErrors()),
+            Messages.ProjectAction_Errors(), label);
+            */
+    }
+    return dataSet;
+  }
+
+
+  /*
+  public void doErrorsGraph(StaplerRequest request, StaplerResponse response)
+          throws IOException {
+      PerformanceReportPosition performanceReportPosition = new PerformanceReportPosition();
+      request.bindParameters(performanceReportPosition);
+      String performanceReportNameFile = performanceReportPosition.getPerformanceReportPosition();
+      if (performanceReportNameFile == null) {
+          if (getPerformanceReportList().size() == 1) {
+              performanceReportNameFile = getPerformanceReportList().get(0);
+          } else {
+              return;
+          }
+      }
+      if (ChartUtil.awtProblemCause != null) {
+          // not available. send out error message
+          response.sendRedirect2(request.getContextPath() + "/images/headless.png");
+          return;
+      }
+      DataSetBuilder<String, ChartUtil.NumberOnlyBuildLabel> dataSetBuilderErrors = new DataSetBuilder<String, ChartUtil.NumberOnlyBuildLabel>();
+      List<? extends AbstractBuild<?, ?>> builds = getProject().getBuilds();
+      Range buildsLimits = getFirstAndLastBuild(request, builds);
+
+      int nbBuildsToAnalyze = builds.size();
+      for (AbstractBuild<?, ?> currentBuild :builds) {
+          if (buildsLimits.in(nbBuildsToAnalyze)) {
+
+              if (!buildsLimits.includedByStep(currentBuild.number)){
+                  continue;
+              }
+
+              ChartUtil.NumberOnlyBuildLabel label = new ChartUtil.NumberOnlyBuildLabel(currentBuild);
+              PerformanceBuildAction performanceBuildAction = currentBuild.getAction(PerformanceBuildAction.class);
+              if (performanceBuildAction == null) {
+                  continue;
+              }
+              PerformanceReport performanceReport = performanceBuildAction.getPerformanceReportMap().getPerformanceReport(
+                      performanceReportNameFile);
+              if (performanceReport == null) {
+                  nbBuildsToAnalyze--;
+                  continue;
+              }
+              dataSetBuilderErrors.add(performanceReport.errorPercent(),
+                      Messages.ProjectAction_Errors(), label);
+          }
+          nbBuildsToAnalyze--;
+      }
+      ChartUtil.generateGraph(request, response,
+              createErrorsChart(dataSetBuilderErrors.build()), 400, 200);
+  }
+  */
 }
